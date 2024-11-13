1a42b6d283977873f58cc63d2a6a37c69ead8383
Azathothas <AjamX101@gmail.com>
2024-11-13 13:44:42 +0545

    AIO

--- .github/scripts/gen_sync_aarch64-Linux.sh
+++ .github/scripts/gen_sync_aarch64-Linux.sh
@@ -63,7 +63,7 @@ if [ "$(jq '. | length' "${SYSTMP}/METADATA.json.src")" -gt 1000 ]; then
      "src_url = \"\(.repo_url)\"\n" +
      "homepage = \"\(.web_url)\"\n" +
      "build_script = \"\(.build_script)\"\n" +
-     "build_log = \"https://bincache.pkgforge.dev/x86_64/" + (if (.build_script | type) == "string" and (.build_script | test(".*/.*\\..+")) then (.build_script | split("/") | last | split(".") | .[0]) else .name end) + ".log\"\n" +
+     "build_log = \"https://bincache.pkgforge.dev/aarch64/" + (if (.build_script | type) == "string" and (.build_script | test(".*/.*\\..+")) then (.build_script | split("/") | last | split(".") | .[0]) else .name end) + ".log\"\n" +
      "category = \"" + (if .repo_topics == "" then "utility" else .repo_topics end) + "\"\n" +
      "icon = \"" + (if .icon == null or .icon == "" then "https://bincache.pkgforge.dev/aarch64/base.default.png" else .icon end) + "\"\n" +
      "provides = \"\(.extra_bins)\"\n" +
@@ -71,6 +71,40 @@ if [ "$(jq '. | length' "${SYSTMP}/METADATA.json.src")" -gt 1000 ]; then
        ( .snapshots // [] | map("\"" + . + "\"") | join(", ")) +
      "]\n"
    ' 2>/dev/null >> "${SYSTMP}/aarch64-Linux-METADATA.toml"
+   ##Pkgs (pkg.pkgforge.dev)
+   curl -qfsSL "https://pkgcache.pkgforge.dev/aarch64/METADATA.min.json" |\
+   jq -r '
+     .[] |
+     "[[pkg]]\n" +
+     "pkg = \"\(.name)\"\n" +
+     "pkg_id = \"\(.bin_id)\"\n" +
+     "pkg_name = \"\(.bin_name)\"\n" +
+     "description = \"\(.description)\"\n" +
+     "note = \"\(.note)\"\n" +
+     "version = \"\(.version)\"\n" +
+     "download_url = \"\(.download_url)\"\n" +
+     "size = \"\(.size | ascii_upcase)\"\n" +
+     "bsum = \"\(.bsum)\"\n" +
+     "shasum = \"\(.shasum)\"\n" +
+     "build_date = \"\(.build_date)\"\n" +
+     "repology = \"\(.repology)\"\n" +
+     "src_url = \"\(.src_url)\"\n" +
+     "homepage = \"\(.web_url)\"\n" +
+     "build_script = \"\(.build_script)\"\n" +
+     "build_log = \"\(.build_log)\"\n" +
+     "appstream = \"\(.appstream)\"\n" +
+     "category = \"\(.category)\"\n" +
+     "desktop = \"\(.desktop)\"\n" +
+     "icon = \"" + (if .icon == null or .icon == "" then "https://pkgcache.pkgforge.dev/aarch64/pkg.default.png" else .icon end) + "\"\n" +
+     "screenshots = [" +
+       ( .screenshots // [] | map("\"" + . + "\"") | join(", ")) +
+     "]\n" +
+     "provides = \"\(.provides)\"\n" +
+     "snapshots = [" +
+       ( .snapshots // [] | map("\"" + . + "\"") | join(", ")) +
+     "]\n"
+   ' 2>/dev/null >> "${SYSTMP}/aarch64-Linux-METADATA.toml"
+   ##Validate & Merge
    validtoml "${SYSTMP}/aarch64-Linux-METADATA.toml"
    taplo check "${SYSTMP}/aarch64-Linux-METADATA.toml" && cp "${SYSTMP}/aarch64-Linux-METADATA.toml" "${TMPDIR}/METADATA.AIO.toml"
    cat "${TMPDIR}/METADATA.AIO.toml" | yj -tj | jq 'to_entries | sort_by(.key) | from_entries' > "${TMPDIR}/METADATA.AIO.json"
@@ -133,7 +167,8 @@ if jq --exit-status . "${TMPDIR}/METADATA.json" >/dev/null 2>&1; then
       fi
     fi
     #Merge
-     jq -s '{"base": .[0], "bin": .[1]}' "${TMPDIR}/METADATA.json.base" "${TMPDIR}/METADATA.json.bin" > "${TMPDIR}/METADATA.json.snaps"
+     jq '.pkg' "${TMPDIR}/METADATA.json" > "${TMPDIR}/METADATA.json.pkg"
+     jq -s '{"base": .[0], "bin": .[1], "pkg": .[2]}' "${TMPDIR}/METADATA.json.base" "${TMPDIR}/METADATA.json.bin" "${TMPDIR}/METADATA.json.pkg" > "${TMPDIR}/METADATA.json.snaps"
     #Validate 
      if jq --exit-status . "${TMPDIR}/METADATA.json.snaps" >/dev/null 2>&1; then
         cat "${TMPDIR}/METADATA.json.snaps" > "${TMPDIR}/METADATA.json"
@@ -143,13 +178,28 @@ if jq --exit-status . "${TMPDIR}/METADATA.json" >/dev/null 2>&1; then
    #Sync
     if jq --exit-status . "${TMPDIR}/METADATA.json" >/dev/null 2>&1; then
      git sparse-checkout set ""
-     git sparse-checkout set --no-cone --sparse-index "${HOST_TRIPLET}/*.json" "${HOST_TRIPLET}/*.log" "${HOST_TRIPLET}/*.png" "${HOST_TRIPLET}/*.temp" "${HOST_TRIPLET}/*.toml" "${HOST_TRIPLET}/*.tmp" "${HOST_TRIPLET}/*.txt" "${HOST_TRIPLET}/*.yaml" "${HOST_TRIPLET}/*.yml"
+     git sparse-checkout set --no-cone --sparse-index "${HOST_TRIPLET}/*.bsum" "${HOST_TRIPLET}/*.json" "${HOST_TRIPLET}/*.log" "${HOST_TRIPLET}/*.png" "${HOST_TRIPLET}/*.temp" "${HOST_TRIPLET}/*.toml" "${HOST_TRIPLET}/*.tmp" "${HOST_TRIPLET}/*.txt" "${HOST_TRIPLET}/*.xz" "${HOST_TRIPLET}/*.yaml" "${HOST_TRIPLET}/*.yml" "${HOST_TRIPLET}/*.zstd"
      git checkout ; ls -lah "." "./${HOST_TRIPLET}"
      find "${HF_REPO_LOCAL}" -type f -size -3c -delete
      #rm "./${HOST_TRIPLET}/METADATA.json.tmp" 2>/dev/null
+     find "${HF_REPO_LOCAL}/${HOST_TRIPLET}" -type f -iname "*.capnp*" -exec rm "{}" -rf \; 2>/dev/null
+     find "${HF_REPO_LOCAL}/${HOST_TRIPLET}" -type f -iname "*.toml*" -exec rm "{}" -rf \; 2>/dev/null
+     find "${HF_REPO_LOCAL}/${HOST_TRIPLET}" -type f -iname "*.yaml*" -exec rm "{}" -rf \; 2>/dev/null
+     find "${HF_REPO_LOCAL}/${HOST_TRIPLET}" -type f -iname "*.min.json*" -exec rm "{}" -rf \; 2>/dev/null
      cp -fv "${TMPDIR}/METADATA.json" "${HF_REPO_LOCAL}/${HOST_TRIPLET}/METADATA.json"
-     #jq -r tostring "${TMPDIR}/METADATA.json" > "${HF_REPO_LOCAL}/${HOST_TRIPLET}/METADATA.min.json"
+     cp -fv "${HF_REPO_LOCAL}/${HOST_TRIPLET}/METADATA.json" "${HF_REPO_LOCAL}/${HOST_TRIPLET}/METADATA.AIO.json"
      cat "${HF_REPO_LOCAL}/${HOST_TRIPLET}/METADATA.json" | jq -r tostring > "${HF_REPO_LOCAL}/${HOST_TRIPLET}/METADATA.min.json"
+     cp -fv "${HF_REPO_LOCAL}/${HOST_TRIPLET}/METADATA.min.json" "${HF_REPO_LOCAL}/${HOST_TRIPLET}/METADATA.AIO.min.json"
+     b3sum "${HF_REPO_LOCAL}/${HOST_TRIPLET}/METADATA.json" | grep -oE '^[a-f0-9]{64}' | tr -d '[:space:]' > "${HF_REPO_LOCAL}/${HOST_TRIPLET}/METADATA.json.bsum"
+     cp -fv "${HF_REPO_LOCAL}/${HOST_TRIPLET}/METADATA.json.bsum" "{HF_REPO_LOCAL}/${HOST_TRIPLET}/METADATA.AIO.json.bsum"
+     7z a -t7z -mx="9" -mmt="$(($(nproc)+1))" -bsp1 -bt "${HF_REPO_LOCAL}/${HOST_TRIPLET}/METADATA.json.xz" "${HF_REPO_LOCAL}/${HOST_TRIPLET}/METADATA.json" 2>/dev/null
+     cp -fv "${HF_REPO_LOCAL}/${HOST_TRIPLET}/METADATA.json.xz" "${HF_REPO_LOCAL}/${HOST_TRIPLET}/METADATA.AIO.json.xz"
+     b3sum "${HF_REPO_LOCAL}/${HOST_TRIPLET}/METADATA.json.xz" | grep -oE '^[a-f0-9]{64}' | tr -d '[:space:]' > "${HF_REPO_LOCAL}/${HOST_TRIPLET}/METADATA.json.xz.bsum"
+     cp -fv "${HF_REPO_LOCAL}/${HOST_TRIPLET}/METADATA.json.xz.bsum" "${HF_REPO_LOCAL}/${HOST_TRIPLET}/METADATA.AIO.json.xz.bsum"
+     zstd --ultra -22 --force "${HF_REPO_LOCAL}/${HOST_TRIPLET}/METADATA.json" -o "${HF_REPO_LOCAL}/${HOST_TRIPLET}/METADATA.json.zstd"
+     cp -fv "${HF_REPO_LOCAL}/${HOST_TRIPLET}/METADATA.json.zstd" "${HF_REPO_LOCAL}/${HOST_TRIPLET}/METADATA.AIO.json.zstd"
+     b3sum "${HF_REPO_LOCAL}/${HOST_TRIPLET}/METADATA.json.zstd" | grep -oE '^[a-f0-9]{64}' | tr -d '[:space:]' > "${HF_REPO_LOCAL}/${HOST_TRIPLET}/METADATA.json.zstd.bsum"
+     cp -fv "${HF_REPO_LOCAL}/${HOST_TRIPLET}/METADATA.json.zstd.bsum" "${HF_REPO_LOCAL}/${HOST_TRIPLET}/METADATA.AIO.json.zstd.bsum"
    #Commit & Push
      git add --all --verbose && git commit -m "[+] METADATA (${HOST_TRIPLET}) [$(TZ='UTC' date +'%Y_%m_%d')]"
      git branch -a || git show-branch
